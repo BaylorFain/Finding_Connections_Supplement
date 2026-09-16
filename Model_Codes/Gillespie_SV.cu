7f98cb61b83d533c6605dc1840297cce63105397
Baylor Fain <46724331+BaylorFain@users.noreply.github.com>
2026-09-15 20:36:13 -0600

    Update Gillespie_SV.cu

--- Model_Codes/Gillespie_SV.cu
+++ Model_Codes/Gillespie_SV.cu
@@ -182,7 +182,7 @@ for (int i = 0; i < n_simulations; i++){
         D += ND;
         V += NV;
 
-        if (floor(Time) >= savetime[count]){
+        if (Time >= savetime[count]){
 
             FILE *outfile1 = fopen(File1,"a");
             if (outfile1 == NULL){
@@ -190,7 +190,7 @@ for (int i = 0; i < n_simulations; i++){
                 exit(0);
             }
 
-            fprintf(outfile1,"%.0f, %d, %d, %d, %d, %.10f", floor(Time), T, E, I, D, V/scale);
+            fprintf(outfile1,"%.10f, %d, %d, %d, %d, %.10f", Time, T, E, I, D, V/scale);
             fprintf(outfile1,"\n");
             fclose(outfile1);
             count += 1;
